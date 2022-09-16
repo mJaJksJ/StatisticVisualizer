f791b0c02198b954f232376482c6c31cd0267bb4
mJaJksJ <maksim.m00@mail.ru>
2022-09-16 23:06:30 +0300

    chore: добавил хедер и футер

--- StatisticVisualizer/ClientApp/src/App.js
+++ StatisticVisualizer/ClientApp/src/App.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <>
+        <header>
+          <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light border-bottom box-shadow mb-3" style={{ 'backgroundColor': '#148F2A' }}>
+            <div className="container-fluid">
+              <a className="navbar-brand text-white">StatisticVisualizer</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                <ul className="navbar-nav flex-grow-1">
+                  <li className="nav-item">
+                    <a className="nav-link text-white">Загрузить файл</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-white">Статистика</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <div class="container">
+          <main role="main" class="pb-3">
+          </main>
+        </div>
+
+        <footer class="border-top footer text-muted">
+          <div class="container">
+            &copy; 2022 - StatisticVisualizer - <a href="https://github.com/mJaJksJ">mJaJksJ</a>
+          </div>
+        </footer>
+      </>
     );
   }
 }
