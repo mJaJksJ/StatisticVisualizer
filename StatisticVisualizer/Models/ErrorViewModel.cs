54aea4dcddd99b084fad88beda1ddcb8e5f33d91
mJaJksJ <maksim.m00@mail.ru>
2022-09-16 10:31:17 +0300

    init project

---
+++ StatisticVisualizer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StatisticVisualizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
